c4f0fe0e72ae5562ee8752e6a6cd36364539be39
Josh Woodcock <josh@j0w0.com>
2020-09-22 10:00:01 -0500

    API access token

--- main_app/api.py
+++ main_app/api.py
@@ -1,55 +1,54 @@
 import os
 import requests
 
-PETFINDER_TOKEN = os.getenv("PETFINDER_TOKEN")
+# PETFINDER_TOKEN = os.getenv("PETFINDER_TOKEN")
 PETFINDER_CLIENT_ID = os.getenv("PETFINDER_CLIENT_ID")
 PETFINDER_CLIENT_SECRET = os.getenv("PETFINDER_CLIENT_SECRET")
 
 
 def get_animals():
     endpoint = 'https://api.petfinder.com/v2/animals'
-    data = {}
-    headers = {'Authorization': 'Bearer ' + PETFINDER_TOKEN}
-    response = requests.request('get', endpoint, headers=headers, data=data)
-    return response.json()
+    return base_request(endpoint)
 
 
 def get_animal(animal_id):
     endpoint = f'https://api.petfinder.com/v2/animals/{animal_id}'
-    data = {}
-    headers = {'Authorization': 'Bearer ' + PETFINDER_TOKEN}
-    response = requests.request('get', endpoint, headers=headers, data=data)
-    return response.json()
+    return base_request(endpoint)
 
 
 def get_animal_types():
     endpoint = 'https://api.petfinder.com/v2/types'
-    data = {}
-    print(PETFINDER_TOKEN)
-    headers = {'Authorization': 'Bearer ' + PETFINDER_TOKEN}
-    response = requests.request('get', endpoint, headers=headers, data=data)
-    return response.json()
+    return base_request(endpoint)
 
 
 def get_animals_type(animal_type):
     endpoint = f'https://api.petfinder.com/v2/types/{animal_type}'
-    data = {}
-    headers = {'Authorization': 'Bearer ' + PETFINDER_TOKEN}
-    response = requests.request('get', endpoint, headers=headers, data=data)
-    return response.json()
+    return base_request(endpoint)
 
 
 def get_animal_breed(animal_type):
     endpoint = f'https://api.petfinder.com/v2/types/{animal_type}/breeds'
-    data = {}
-    headers = {'Authorization': 'Bearer ' + PETFINDER_TOKEN}
-    response = requests.request('get', endpoint, headers=headers, data=data)
-    return response.json()
+    return base_request(endpoint)
 
 
 def filter_animals(query_string):
     endpoint = f'https://api.petfinder.com/v2/animals{query_string}'
+    return base_request(endpoint)
+
+
+def base_request(endpoint):
     data = {}
-    headers = {'Authorization': 'Bearer ' + PETFINDER_TOKEN}
-    response = requests.request('get', endpoint, headers=headers, data=data)
+    headers = {'Authorization': 'Bearer ' + get_access_token()}
+    response = requests.request('GET', endpoint, headers=headers, data=data)
     return response.json()
+
+
+def get_access_token():
+    endpoint = 'https://api.petfinder.com/v2/oauth2/token'
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': PETFINDER_CLIENT_ID,
+        'client_secret': PETFINDER_CLIENT_SECRET
+    }
+    response = requests.post(endpoint, data=data)
+    return response.json()['access_token']
